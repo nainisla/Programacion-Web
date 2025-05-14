fe3aec5147e0dc9dd49cc6bc05b00154816e59f2
PriAnto06 <priscilaantonella.d.07@gmail.com>
2025-05-14 17:00:07 -0300

    hacer una lista de compras

---
+++ Ejercicio DOM/script.js
@@ -0,0 +1,19 @@
+let lista = [];
+
+function addTask() {
+  const input = document.getElementById("input-task");
+  const text = input.value;
+  console.log(text);
+
+  const obj = { task: text, completed: false };
+  if (text !== "") {
+    lista.push(obj);
+  }
+
+  const list = document.getElementById("task-list");
+
+  //crear un elemento html
+  const li = document.createElement("li");
+  li.textContent = obj.task;
+  list.append(li);
+}
